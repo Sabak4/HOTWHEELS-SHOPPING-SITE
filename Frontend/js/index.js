c772262c77e1efb3dcad214f6e8c5b5a54d3794b
Soumya Basak <soumyabasak390@gmail.com>
2023-07-08 08:08:30 +0530

    ADDED THE SIGN UP PAGE
    Here added the second Sign Up page with basic css and also implemented going to signup page from login page and vice versa.

---
+++ Frontend/js/index.js
@@ -0,0 +1,4 @@
+document.querySelector(".accountCreation").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "signup.html";
+})
---
+++ Frontend/js/signup.js
@@ -0,0 +1,5 @@
+document.querySelector(".loginRedirect").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "index.html";
+})
+
