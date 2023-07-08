8b3fcf513627eb94e0c4f70aac3832f9daff4200
Soumya Basak <soumyabasak390@gmail.com>
2023-07-08 11:39:07 +0530

    ADDED THE HOMEPAGE
    When the login is successful from "index.html" page we are redirected to the "homepage.html"

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,4 +1,11 @@
+// If the user doesn't have an account clicking on it will take to the sign up page
 document.querySelector(".accountCreation").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "signup.html";
+})
+
+//On successfull login it will take to the user to the homepage
+document.querySelector(".loginButton").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "homepage.html";
 })
--- Frontend/js/signup.js
+++ Frontend/js/signup.js
@@ -1,4 +1,11 @@
+// This will redirect to the index.html page when an click is registered
 document.querySelector(".loginRedirect").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "index.html";
+})
+
+// On successfull Sign Up it will also redirect to the Login Page
+document.querySelector(".signUpButton").addEventListener("click", function(event) {
     event.preventDefault();
     window.location.href = "index.html";
 })
