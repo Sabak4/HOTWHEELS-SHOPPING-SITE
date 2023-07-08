ef4cbde0f755d4d16373f3d90109c989e30b79e6
Soumya Basak <soumyabasak390@gmail.com>
2023-07-08 15:21:35 +0530

    ADDED CONNECTION FROM THE CAROUSEL TO THE RESPECTIVE PAGES
    Added the connection so when we are clicking on the Carousel we will be redirected to the webpage the image is part of.  Like when we are clicking on "Sports Hotwheels" we will be directed to the "sports_hotwheels_page.html".

--- Frontend/js/homepage.js
+++ Frontend/js/homepage.js
@@ -2,3 +2,29 @@ document.querySelector(".cart").addEventListener("click",function(event) {
     event.preventDefault();
     window.location.href = "cart.html";
 })
+
+// These 4 functions will redirect to the page where we can add the items to the card
+
+// Redirect from the classic hotwheels carousel to the classic hotwheels page
+document.querySelector(".classicHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "html/classic_hotwheels_page.html";
+})
+
+// Redirect from the Sports Hotwheels to the Sports Hotwheels Page
+document.querySelector(".sportsHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "html/sports_hotwheels_page.html";
+})
+
+// Redirect from the Sports Hotwheels to the Truck Hotwheels Page
+document.querySelector(".truckHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "html/truck_hotwheels_page.html";
+})
+
+// Redirect from the Sports Hotwheels to the Batmobile Hotwheels Page
+document.querySelector(".batmanHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "html/batmobile_hotwheels_page.html";
+})
