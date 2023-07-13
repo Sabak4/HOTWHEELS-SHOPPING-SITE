6c4f2703f6388c7b7836b60c8da379f8191850a5
Soumya Basak <soumyabasak390@gmail.com>
2023-07-13 08:11:46 +0530

    ADDED THE HTML CSS FOR CLASSIC HOTWHEELS PAGE
    Added the HTML, CSS for the Classic Hotwheels Page and also set up the connection of all the hotwheels page with there respective CSS and JS.

---
+++ Frontend/js/batmobile_hotwheels_page.js
@@ -0,0 +1,36 @@
+document.querySelector(".cart").addEventListener("click",function(event) {
+    event.preventDefault();
+    window.location.href = "../cart.html";
+})
+
+// These 4 functions will redirect to the page where we can add the items to the card
+
+// Redirect from the classic hotwheels carousel to the classic hotwheels page
+document.querySelector(".classicHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "classic_hotwheels_page.html";
+})
+
+// Redirect from the Sports Hotwheels to the Sports Hotwheels Page
+document.querySelector(".sportsHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "sports_hotwheels_page.html";
+})
+
+// Redirect from the Sports Hotwheels to the Truck Hotwheels Page
+document.querySelector(".truckHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "truck_hotwheels_page.html";
+})
+
+// Redirect from the Sports Hotwheels to the Batmobile Hotwheels Page
+document.querySelector(".batmanHotwheelsRedirect").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "batmobile_hotwheels_page.html";
+})
+
+//Redirect to the Homepage from Batmobile Page
+document.querySelector(".header_button").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "../homepage.html"
+})
