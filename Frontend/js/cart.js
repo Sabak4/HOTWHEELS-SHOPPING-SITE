efa7fcda946464ee0be014576172a78c5cde06b8
Soumya Basak <soumyabasak390@gmail.com>
2023-07-18 09:05:56 +0530

    ADDED FUNCTIONALITY TO CART PAGE
    Now if we are adding any items from the "classic_hotwheels_page.html" and clicking on the Cart icon on the html page it will be redirected to the "cart.html" page and there if will show the item which we have added.

--- Frontend/js/cart.js
+++ Frontend/js/cart.js
@@ -1,4 +1,4 @@
-document.querySelector(".cart").addEventListener("click",function(event) {
+document.querySelector(".cart").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "cart.html";
 })
@@ -6,31 +6,62 @@ document.querySelector(".cart").addEventListener("click",function(event) {
 // These 4 functions will redirect to the page where we can add the items to the card
 
 // Redirect from the Cart Page to the classic hotwheels page
-document.querySelector(".classicHotwheelsRedirect").addEventListener("click", function(event) {
+document.querySelector(".classicHotwheelsRedirect").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "html/classic_hotwheels_page.html";
 })
 
 // Redirect from the Cart Page to the Sports Hotwheels Page
-document.querySelector(".sportsHotwheelsRedirect").addEventListener("click", function(event) {
+document.querySelector(".sportsHotwheelsRedirect").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "html/sports_hotwheels_page.html";
 })
 
 // Redirect from the Cart Page to the Truck Hotwheels Page
-document.querySelector(".truckHotwheelsRedirect").addEventListener("click", function(event) {
+document.querySelector(".truckHotwheelsRedirect").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "html/truck_hotwheels_page.html";
 })
 
 // Redirect from the Cart Page to the Batmobile Hotwheels Page
-document.querySelector(".batmanHotwheelsRedirect").addEventListener("click", function(event) {
+document.querySelector(".batmanHotwheelsRedirect").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "html/batmobile_hotwheels_page.html";
 })
 
 // Redirect from Cart Page to Homepage
-document.querySelector(".homePageRedirect").addEventListener("click", function(event) {
+document.querySelector(".homePageRedirect").addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "homepage.html";
 })
+
+// This checking is for Classic cars 
+var htmlcode = "";
+for (var i = 0; i < sessionStorage.length; i++) {
+
+    var keySession = sessionStorage.key(i);
+    var valueSession = sessionStorage.getItem(keySession);
+
+    if (valueSession > 0) {
+
+
+        var imageURL = "../images/classic_cars/" + keySession + ".jpg";
+        htmlcode = htmlcode + `
+        <div class="cart_body_image">
+            <img src="${imageURL}" alt="${keySession}" >
+        </div>
+        <div class="cart_body_button">
+            <Button class="add_quantity_button">Remove</BUtton>
+            <Button class="add_quantity_button">Add</Button>
+        </div>
+        <div class="cart_body_quantity">
+            ${valueSession}
+        </div>`;
+    }
+}
+document.querySelector(".cart_flex_container").innerHTML = htmlcode;
+
+document.querySelector(".add_quantity_button").addEventListener("click", function(event) {
+    
+})
+
