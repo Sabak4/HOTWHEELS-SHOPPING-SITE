133731c834dbad1f737efa22c3822ddb159223e5
Soumya Basak <soumyabasak390@gmail.com>
2023-07-19 17:34:28 +0530

    ADDED THE BUTTOM TO REDUCE THE QUANTITY IN CART PAGE
    Added the "-" button functionality which will reduce the quantity of the item added in the cart page.

--- Frontend/js/cart.js
+++ Frontend/js/cart.js
@@ -497,6 +497,10 @@ document.querySelectorAll(".remove_quantity_button").forEach(function (button) {
 
         // Here we are checking if we can find the classname from the list according to it adding the value in the sessionManager
 
+        /*
+        This is for the Classic Cars
+        */
+
         // This check is for Pontiac_Firebird_67_400
         if (list_of_class_add_button.includes('Pontiac_Firebird_67_400')) {
             var count = sessionStorage.getItem('Pontiac_Firebird_67_400');
@@ -625,7 +629,401 @@ document.querySelectorAll(".remove_quantity_button").forEach(function (button) {
             }
         }
 
+        /*
+        This is for the Sports Cars
+        */
 
+        // This check is for Pontiac_Firebird_67_400
+        if (list_of_class_add_button.includes('BMW_E36_M3_Race')) {
+            var count = sessionStorage.getItem('BMW_E36_M3_Race');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('BMW_E36_M3_Race', count);
+                console.log(sessionStorage.getItem('BMW_E36_M3_Race'));
+                document.querySelector(".cart_body_quantity.BMW_E36_M3_Race").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of BMW_E36_M3_Race is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Bugatti_Chiron_Blue_16
+        if (list_of_class_add_button.includes('Bugatti_Chiron_Blue_16')) {
+            var count = sessionStorage.getItem('Bugatti_Chiron_Blue_16');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Bugatti_Chiron_Blue_16', count);
+                console.log(sessionStorage.getItem('Bugatti_Chiron_Blue_16'));
+                document.querySelector(".cart_body_quantity.Bugatti_Chiron_Blue_16").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Bugatti_Chiron_Blue_16 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Bugatti_Veyron
+        if (list_of_class_add_button.includes('Bugatti_Veyron')) {
+            var count = sessionStorage.getItem('Bugatti_Veyron');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Bugatti_Veyron', count);
+                console.log(sessionStorage.getItem('Bugatti_Veyron'));
+                document.querySelector(".cart_body_quantity.Bugatti_Veyron").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Bugatti_Veyron is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Lamborghini_Countach
+        if (list_of_class_add_button.includes('Lamborghini_Countach')) {
+            var count = sessionStorage.getItem('Lamborghini_Countach');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Lamborghini_Countach', count);
+                console.log(sessionStorage.getItem('Lamborghini_Countach'));
+                document.querySelector(".cart_body_quantity.Lamborghini_Countach").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Lamborghini_Countach is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Porshe_935
+        if (list_of_class_add_button.includes('Porshe_935')) {
+            var count = sessionStorage.getItem('Porshe_935');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Porshe_935', count);
+                console.log(sessionStorage.getItem('Porshe_935'));
+                document.querySelector(".cart_body_quantity.Porshe_935").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Porshe_935 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for RRRoadsteer
+        if (list_of_class_add_button.includes('RRRoadsteer')) {
+            var count = sessionStorage.getItem('RRRoadsteer');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('RRRoadsteer', count);
+                console.log(sessionStorage.getItem('RRRoadsteer'));
+                document.querySelector(".cart_body_quantity.RRRoadsteer").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of RRRoadsteer is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for SRT_Viper_GTS_R
+        if (list_of_class_add_button.includes('SRT_Viper_GTS_R')) {
+            var count = sessionStorage.getItem('SRT_Viper_GTS_R');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('SRT_Viper_GTS_R', count);
+                console.log(sessionStorage.getItem('SRT_Viper_GTS_R'));
+                document.querySelector(".cart_body_quantity.SRT_Viper_GTS_R").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of SRT_Viper_GTS_R is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Super_Blitzen_Green
+        if (list_of_class_add_button.includes('Super_Blitzen_Green')) {
+            var count = sessionStorage.getItem('Super_Blitzen_Green');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Super_Blitzen_Green', count);
+                console.log(sessionStorage.getItem('Super_Blitzen_Green'));
+                document.querySelector(".cart_body_quantity.Super_Blitzen_Green").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Super_Blitzen_Green is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        /*
+        This is for the Motorcycle
+        */
+
+        // This check is for Kawasaki_Ninja_GPZ_900R
+        if (list_of_class_add_button.includes('Kawasaki_Ninja_GPZ_900R')) {
+            var count = sessionStorage.getItem('Kawasaki_Ninja_GPZ_900R');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Kawasaki_Ninja_GPZ_900R', count);
+                console.log(sessionStorage.getItem('Kawasaki_Ninja_GPZ_900R'));
+                document.querySelector(".cart_body_quantity.Kawasaki_Ninja_GPZ_900R").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Kawasaki_Ninja_GPZ_900R is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Honda_CB750_Cafe
+        if (list_of_class_add_button.includes('Honda_CB750_Cafe')) {
+            var count = sessionStorage.getItem('Honda_CB750_Cafe');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Honda_CB750_Cafe', count);
+                console.log(sessionStorage.getItem('Honda_CB750_Cafe'));
+                document.querySelector(".cart_body_quantity.Honda_CB750_Cafe").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Honda_CB750_Cafe is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Harley_Davidson_Fat_Boy_2012
+        if (list_of_class_add_button.includes('Harley_Davidson_Fat_Boy_2012')) {
+            var count = sessionStorage.getItem('Harley_Davidson_Fat_Boy_2012');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Harley_Davidson_Fat_Boy_2012', count);
+                console.log(sessionStorage.getItem('Harley_Davidson_Fat_Boy_2012'));
+                document.querySelector(".cart_body_quantity.Harley_Davidson_Fat_Boy_2012").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Harley_Davidson_Fat_Boy_2012 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Ducati_Diavel_2013
+        if (list_of_class_add_button.includes('Ducati_Diavel_2013')) {
+            var count = sessionStorage.getItem('Ducati_Diavel_2013');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Ducati_Diavel_2013', count);
+                console.log(sessionStorage.getItem('Ducati_Diavel_2013'));
+                document.querySelector(".cart_body_quantity.Ducati_Diavel_2013").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Ducati_Diavel_2013 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Ducati_DesertX
+        if (list_of_class_add_button.includes('Ducati_DesertX')) {
+            var count = sessionStorage.getItem('Ducati_DesertX');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Ducati_DesertX', count);
+                console.log(sessionStorage.getItem('Ducati_DesertX'));
+                document.querySelector(".cart_body_quantity.Ducati_DesertX").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Ducati_DesertX is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Ducati_1199_Panigale
+        if (list_of_class_add_button.includes('Ducati_1199_Panigale')) {
+            var count = sessionStorage.getItem('Ducati_1199_Panigale');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Ducati_1199_Panigale', count);
+                console.log(sessionStorage.getItem('Ducati_1199_Panigale'));
+                document.querySelector(".cart_body_quantity.Ducati_1199_Panigale").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Ducati_1199_Panigale is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Deadpool_Scooter
+        if (list_of_class_add_button.includes('Deadpool_Scooter')) {
+            var count = sessionStorage.getItem('Deadpool_Scooter');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Deadpool_Scooter', count);
+                console.log(sessionStorage.getItem('Deadpool_Scooter'));
+                document.querySelector(".cart_body_quantity.Deadpool_Scooter").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Deadpool_Scooter is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batpod_2008
+        if (list_of_class_add_button.includes('Batpod_2008')) {
+            var count = sessionStorage.getItem('Batpod_2008');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batpod_2008', count);
+                console.log(sessionStorage.getItem('Batpod_2008'));
+                document.querySelector(".cart_body_quantity.Batpod_2008").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batpod_2008 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        /*
+        This is for the Batmobile
+        */
+
+        // This check is for Batmobile_1966_Tv_Series
+        if (list_of_class_add_button.includes('Batmobile_1966_Tv_Series')) {
+            var count = sessionStorage.getItem('Batmobile_1966_Tv_Series');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_1966_Tv_Series', count);
+                console.log(sessionStorage.getItem('Batmobile_1966_Tv_Series'));
+                document.querySelector(".cart_body_quantity.Batmobile_1966_Tv_Series").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_1966_Tv_Series is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_Arkham_Knight_2015
+        if (list_of_class_add_button.includes('Batmobile_Arkham_Knight_2015')) {
+            var count = sessionStorage.getItem('Batmobile_Arkham_Knight_2015');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_Arkham_Knight_2015', count);
+                console.log(sessionStorage.getItem('Batmobile_Arkham_Knight_2015'));
+                document.querySelector(".cart_body_quantity.Batmobile_Arkham_Knight_2015").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_Arkham_Knight_2015 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_Batman_Forever
+        if (list_of_class_add_button.includes('Batmobile_Batman_Forever')) {
+            var count = sessionStorage.getItem('Batmobile_Batman_Forever');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_Batman_Forever', count);
+                console.log(sessionStorage.getItem('Batmobile_Batman_Forever'));
+                document.querySelector(".cart_body_quantity.Batmobile_Batman_Forever").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_Batman_Forever is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_Batman_The_Animated_Series
+        if (list_of_class_add_button.includes('Batmobile_Batman_The_Animated_Series')) {
+            var count = sessionStorage.getItem('Batmobile_Batman_The_Animated_Series');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_Batman_The_Animated_Series', count);
+                console.log(sessionStorage.getItem('Batmobile_Batman_The_Animated_Series'));
+                document.querySelector(".cart_body_quantity.Batmobile_Batman_The_Animated_Series").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_Batman_The_Animated_Series is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_Batman_v_Superman
+        if (list_of_class_add_button.includes('Batmobile_Batman_v_Superman')) {
+            var count = sessionStorage.getItem('Batmobile_Batman_v_Superman');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_Batman_v_Superman', count);
+                console.log(sessionStorage.getItem('Batmobile_Batman_v_Superman'));
+                document.querySelector(".cart_body_quantity.Batmobile_Batman_v_Superman").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_Batman_v_Superman is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_Justice_League_2018
+        if (list_of_class_add_button.includes('Batmobile_Justice_League_2018')) {
+            var count = sessionStorage.getItem('Batmobile_Justice_League_2018');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_Justice_League_2018', count);
+                console.log(sessionStorage.getItem('Batmobile_Justice_League_2018'));
+                document.querySelector(".cart_body_quantity.Batmobile_Justice_League_2018").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_Justice_League_2018 is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_The_Batman
+        if (list_of_class_add_button.includes('Batmobile_The_Batman')) {
+            var count = sessionStorage.getItem('Batmobile_The_Batman');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_The_Batman', count);
+                console.log(sessionStorage.getItem('Batmobile_The_Batman'));
+                document.querySelector(".cart_body_quantity.Batmobile_The_Batman").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_The_Batman is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
+
+        //This check is for Batmobile_The_Dark_Knight
+        if (list_of_class_add_button.includes('Batmobile_The_Dark_Knight')) {
+            var count = sessionStorage.getItem('Batmobile_The_Dark_Knight');
+            count--;
+            if (count >= 0) {
+                sessionStorage.setItem('Batmobile_The_Dark_Knight', count);
+                console.log(sessionStorage.getItem('Batmobile_The_Dark_Knight'));
+                document.querySelector(".cart_body_quantity.Batmobile_The_Dark_Knight").innerHTML = `
+            ${count}
+            `;
+                console.log("Count of Batmobile_The_Dark_Knight is " + count);
+            } else {
+                alert("Cart item is already Empty cannot delete item");
+            }
+        }
     })
 })
 
